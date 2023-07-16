ba7ca8d7630f1554a1d07a86c4484e61c055357b
donghyuun <bomul0524@gmail.com>
2023-07-16 15:06:36 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import jakarta.persistence.EntityManager;
@@ -12,22 +13,24 @@ import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
-    private EntityManager em;
-    @Autowired
-    public SpringConfig(EntityManager em){//스프링에서 DI해줌
-        this.em = em;
+
+    private final MemberRepository memberRepository;
+
+    @Autowired //생성자가 하나인 경우 생략가능
+    public SpringConfig(MemberRepository memberRepository){//스프링 데이터 Jpa 가 만들어놓은 구현체가 등록됨
+        this.memberRepository = memberRepository;
     }
 
     @Bean//스프링 빈에 등록
     public MemberService memberService(){
-        return new MemberService(memberRepository());//스프링 빈에 등록된 memberRepository를 넣어줌
+        return new MemberService(memberRepository);//스프링 빈에 등록되어 여기서 injection 받은 memberRepository를 넣어줌
     }
 
-    @Bean//스프링 빈에 등록
-    public MemberRepository memberRepository(){
+    //@Bean//스프링 빈에 등록
+    //public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    //}
 }
